1f64b3a022d9ecf9120bf965efe8d9e7752531a0
rishiajay500 <rishiajay500@gmail.com>
2019-08-16 08:17:40 -0400

    I uploaded the files

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burger
+(
+	id int NOT NULL AUTO_INCREMENT,
+	name varchar(255) NOT NULL,
+	eaten BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheddar', true);
+INSERT INTO burgers (name, eaten) VALUES ('Mushroom Onion', true);
+INSERT INTO burgers (name, eaten) VALUES ('Turkey', true);
+INSERT INTO burgers (name) VALUES ('Whiskey River BBQ');
