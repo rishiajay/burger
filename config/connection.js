470fa9e8ee18d0409042284a5eee81a3054581f8
rishiajay500 <rishiajay500@gmail.com>
2019-08-19 08:55:05 -0400

    updated

--- config/connection.js
+++ config/connection.js
@@ -1,32 +1,25 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burger_db"
-});
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+var connection;
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host: 'localhost',
-    user: 'root';
-    password: 'hacktheplanet',
-    database: 'todoagain_db'
+    user: "root",
+    password: "password",
+    database: 'burger_db'
   })
+};
+
+connection.config.typeCast = function(field, next) {
+  if (field.type == "TINY" && field.length == 1) {
+    return field.string() == "1"; // 1 = true, 0 = false
+  }
+  return next();
 };
 
 // Export connection for our ORM to use.
