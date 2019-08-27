5c2c4f19905bae9b70d9015187b105b4fe084356
rishiajay500 <rishiajay500@gmail.com>
2019-08-27 09:55:48 -0400

    deploy

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ var routes = require("./controllers/burgers_controller.js");
 
 app.use(routes);
 
+// test
+
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
