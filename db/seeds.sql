9ea0cb3be8dd261d8654d48e6d8e7d3335bb3184
rishiajay500 <rishiajay500@gmail.com>
2019-09-06 15:24:29 -0400

    did modifications

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
 use burger_db;
-INSERT INTO burger (name) VALUES ('Hamburger');
-INSERT INTO burger (name) VALUES ('Cheeseburger');
-INSERT INTO burger (name, eaten) VALUES ('Bacon Cheddar', true);
-INSERT INTO burger (name, eaten) VALUES ('Mushroom Onion', true);
-INSERT INTO burger (name, eaten) VALUES ('Turkey', true);
-INSERT INTO burger (name) VALUES ('Whiskey River BBQ');
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, eaten) VALUES ('Bacon Cheddar', true);
+INSERT INTO burgers (name, eaten) VALUES ('Mushroom Onion', true);
+INSERT INTO burgers (name, eaten) VALUES ('Turkey', true);
+INSERT INTO burgers (name) VALUES ('Whiskey River BBQ');
